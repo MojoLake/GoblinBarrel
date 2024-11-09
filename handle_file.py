c833a0e7259cdee83bf16487337de28e1b04d4e5
MojoLake <simo.simojoki@gmail.com>
2024-11-09 11:38:46 +0200

    change dock

--- handle_file.py
+++ handle_file.py
@@ -1,14 +1,33 @@
 import ifcopenshell
+import ifcopenshell.geom
 
-file_path = "./data/Ifc2x3_Duplex_Architecture.ifc"
 
+def do_geometry(ifc_file):
+    settings = ifcopenshell.geom.settings()
+    settings.set(settings.USE_WORLD_COORDS, True)
+
+
+    walls = ifc_file.by_type("IfcWallStandardCase")
+
+    wall = walls[0]
+
+    wall_shape = ifcopenshell.geom.create_shape(settings, wall)
+
+    vertices = wall_shape.geometry.verts
+    edges = wall_shape.geometry.edges
+
+    print("Vertices", vertices)
+    print("Edges", edges)
 
 
 def main():
 
+    print("Program starting...")
+    file_path = "./data/Ifc2x3_Duplex_Architecture.ifc"
     demo_file = ifcopenshell.open(file_path)
     walls = demo_file.by_type("IfcWallStandardCase")
-    print("numer of walls foumd:", len(walls))
+    print("number of walls found:", len(walls))
+    do_geometry(demo_file)
 
 
 
